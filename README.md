70bb31a94cd54ed7225e1ab9588ef1ecfd81c858
IvanZhelieznyi <80718169+IvanZhelieznyi@users.noreply.github.com>
2022-05-22 22:58:07 +0300

    Added watermark with upload date

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ blanking-dimension
 https://ivanzhelieznyi.github.io/BlankingDimension/
 
 5.18.2022 -- добавил INTREPID, скопировал и изменил на INTREPID
+5.22.2022 -- добавил водяной знак с датой загрузки
--- js/myjspdf.js
+++ js/myjspdf.js
@@ -1,8 +1,10 @@
 function makePDF() {
+	let now = new Date();
     var originalContents = document.body.innerHTML;
-    var printReport = document.getElementById('c-print').innerHTML;
+    var printReport = document.getElementById('c-print').innerHTML + "<div style=\"color: rgb(255, 204, 0, 0.25); text-align: center; font-weight: bold; font-size: 23px; line-height: 12px; transform: rotate(-25deg); position: absolute; left: 700px; top: 320px; border: rgb(192, 192, 192, 0.25) solid 4px; height: 100px; width: 160px; \"><p>INTERPIPE</p><p>" + (now.getMonth() + 1) + "." + now.getDate() + "." + now.getFullYear() + "</p></div>";
     document.body.innerHTML = printReport;
     window.print();
     document.body.innerHTML = originalContents;
     location.reload();
 }
+
